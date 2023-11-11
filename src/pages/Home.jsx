aa014eb60b4b32dc3f58bb1f69b8ed00511bb7fa
Rokas <rookas.rudzenskas@gmail.com>
2023-11-11 16:20:10 +0200

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,37 +70,37 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-      {/*    <directionalLight position={[1, 1, 1]} intensity={2} />*/}
-      {/*    <ambientLight intensity={0.5} />*/}
-      {/*    <pointLight position={[10, 5, 10]} intensity={2} />*/}
-      {/*    <spotLight*/}
-      {/*      position={[0, 50, 10]}*/}
-      {/*      angle={0.15}*/}
-      {/*      penumbra={1}*/}
-      {/*      intensity={2}*/}
-      {/*    />*/}
-      {/*    <hemisphereLight*/}
-      {/*      skyColor='#b1e1ff'*/}
-      {/*      groundColor='#000000'*/}
-      {/*      intensity={1}*/}
-      {/*    />*/}
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 5, 10]} intensity={2} />
+          <spotLight
+            position={[0, 50, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+          <hemisphereLight
+            skyColor='#b1e1ff'
+            groundColor='#000000'
+            intensity={1}
+          />
 
-      {/*    <Bird />*/}
-      {/*    <Sky isRotating={isRotating} />*/}
-      {/*    <Island*/}
-      {/*      isRotating={isRotating}*/}
-      {/*      setIsRotating={setIsRotating}*/}
-      {/*      setCurrentStage={setCurrentStage}*/}
-      {/*      position={islandPosition}*/}
-      {/*      rotation={[0.1, 4.7077, 0]}*/}
-      {/*      scale={islandScale}*/}
-      {/*    />*/}
-      {/*    <Plane*/}
-      {/*      isRotating={isRotating}*/}
-      {/*      position={biplanePosition}*/}
-      {/*      rotation={[0, 20.1, 0]}*/}
-      {/*      scale={biplaneScale}*/}
-      {/*    />*/}
+          <Bird />
+          <Sky isRotating={isRotating} />
+          <Island
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
+            position={islandPosition}
+            rotation={[0.1, 4.7077, 0]}
+            scale={islandScale}
+          />
+          <Plane
+            isRotating={isRotating}
+            position={biplanePosition}
+            rotation={[0, 20.1, 0]}
+            scale={biplaneScale}
+          />
         </Suspense>
       </Canvas>
 
