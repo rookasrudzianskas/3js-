401e3d5c42bd32350493c72f5aa27066bc8aef75
Rokas <rookas.rudzenskas@gmail.com>
2023-11-11 10:38:51 +0200

    loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -69,7 +69,7 @@ const Home = () => {
         }`}
         camera={{ near: 0.1, far: 1000 }}
       >
-      {/*  <Suspense fallback={<Loader />}>*/}
+        <Suspense fallback={<Loader />}>
       {/*    <directionalLight position={[1, 1, 1]} intensity={2} />*/}
       {/*    <ambientLight intensity={0.5} />*/}
       {/*    <pointLight position={[10, 5, 10]} intensity={2} />*/}
@@ -101,7 +101,7 @@ const Home = () => {
       {/*      rotation={[0, 20.1, 0]}*/}
       {/*      scale={biplaneScale}*/}
       {/*    />*/}
-      {/*  </Suspense>*/}
+        </Suspense>
       </Canvas>
 
       {/*<div className='absolute bottom-2 left-2'>*/}
